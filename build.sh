9a06f5fed6adad698bc5d00013b1ff12df009ecc
flytreeleft <flytreeleft@crazydan.org>
2021-11-14 11:36:45 +0800

    Support to minify js files

--- build.sh
+++ build.sh
@@ -6,17 +6,33 @@ SRC_DIR="${DIR}/src"
 DIST_DIR="${DIR}/dist"
 
 
+try() {
+    "$@" || exit 1
+}
+
 echo
 echo "Build Elm source files ..."
-elm make \
+try elm make \
     --optimize \
     --output="${DIST_DIR}/js/main.js" \
-    "${SRC_DIR}/Main.elm" \
-|| exit 1
+    "${SRC_DIR}/Main.elm"
 
 
 echo
-echo "Copy static file to ${DIST_DIR} ..."
+echo "Minify javascript files ..."
+# https://guide.elm-lang.org/optimization/asset_size.html
+try uglifyjs \
+    --compress \
+    'pure_funcs=[F2,F3,F4,F5,F6,F7,F8,F9,A2,A3,A4,A5,A6,A7,A8,A9],pure_getters,keep_fargs=false,unsafe_comps,unsafe' \
+    "${DIST_DIR}/js/main.js" \
+| try uglifyjs --mangle \
+    --output \
+    "${DIST_DIR}/js/main.min.js" \
+&& rm -f "${DIST_DIR}/js/main.js"
+
+
+echo
+echo "Copy static files to ${DIST_DIR} ..."
 rsync -avP \
-    asset index.html \
+    static/ \
     "${DIST_DIR}"/
