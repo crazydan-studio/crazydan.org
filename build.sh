3d04169c98179085842536a5624b57063d487de1
flytreeleft <flytreeleft@crazydan.org>
2021-11-14 14:07:51 +0800

    Improve build.sh

--- build.sh
+++ build.sh
@@ -4,17 +4,29 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)"
 
 SRC_DIR="${DIR}/src"
 DIST_DIR="${DIR}/dist"
+BUILD_DIR="${DIR}/.build"
+
+MAIN_JS="${BUILD_DIR}/main.js"
+MAIN_MIN_JS="${DIST_DIR}/js/main.min.js"
 
 
 try() {
     "$@" || exit 1
 }
 
+# https://gist.github.com/evancz/fc6ff4995395a1643155593a182e2de7
+if ! [ -x "$(command -v uglifyjs)" ]; then
+  echo 'Error: need uglifyjs to be available for asset size test.'
+  echo 'You can run `npm install --global uglify-js` to get it.'
+  exit 1
+fi
+
+
 echo
 echo "Build Elm source files ..."
 try elm make \
     --optimize \
-    --output="${DIST_DIR}/js/main.js" \
+    --output="${MAIN_JS}" \
     "${SRC_DIR}/Main.elm"
 
 
@@ -24,11 +36,15 @@ echo "Minify javascript files ..."
 try uglifyjs \
     --compress \
     'pure_funcs=[F2,F3,F4,F5,F6,F7,F8,F9,A2,A3,A4,A5,A6,A7,A8,A9],pure_getters,keep_fargs=false,unsafe_comps,unsafe' \
-    "${DIST_DIR}/js/main.js" \
+    "${MAIN_JS}" \
 | try uglifyjs --mangle \
     --output \
-    "${DIST_DIR}/js/main.min.js" \
-&& rm -f "${DIST_DIR}/js/main.js"
+    "${MAIN_MIN_JS}"
+
+echo
+echo "  Initial size: $(cat "${MAIN_JS}" | wc -c) bytes  ($(basename "${MAIN_JS}"))"
+echo "  Minified size: $(cat "${MAIN_MIN_JS}" | wc -c) bytes  ($(basename "${MAIN_MIN_JS}"))"
+echo "  Gzipped size: $(cat "${MAIN_MIN_JS}" | gzip -c | wc -c) bytes"
 
 
 echo
